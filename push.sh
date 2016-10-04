aec36c25014305f8be29ef0ca12971d385ca7fff
Vivek <vivek@shippable.com>
2016-10-04 15:48:28 +0530

    increment

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status -s
 git add .
 git commit -m "increment"
-git push origin http://vivek@ec2-52-91-17-41.compute-1.amazonaws.com:7990/scm/shub/sample_shippable.git master -f
+git push origin git@github.com:ankunath/sample_pipelines_2.git master -f
