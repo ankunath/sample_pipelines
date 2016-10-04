28eb2ae624e3e8872dba3066b3bfaf1d7ecd5541
shubhankit <sshubhankit@ymail.com>
2016-10-04 12:22:30 +0530

    increment

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status -s
 git add .
 git commit -m "increment"
-git push https://ankunath@bitbucket.org/ankunath/shippablesample.git master -f
+git push origin http://vivek@ec2-52-91-17-41.compute-1.amazonaws.com:7990/scm/shub/sample_shippable.git master -f
