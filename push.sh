bba3ce8b92a8f2147b9a23cc5a1d19aa0a8fb9b4
shubhankit <iitianshubhankit@gmail.com>
2017-04-13 12:57:39 +0530

    increment

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status -s
 git add .
 git commit -m "increment"
-git push git@github.com:ankunath/sample_pipelines_2.git master -f
+git push git@github.com:ankunath/sample_pipelines_2.git newB -f
